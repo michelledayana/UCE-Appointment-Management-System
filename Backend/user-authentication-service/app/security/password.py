ff67d8a7794913675a068b63b0155a8a68627359
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 00:21:54 -0500

    🔑 Add security module

---
+++ Backend/user-authentication-service/app/security/password.py
@@ -0,0 +1,6 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
