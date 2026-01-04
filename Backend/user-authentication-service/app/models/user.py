ca8cba6502ad418222f7eef2f8c3c6e351d12645
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 15:44:58 -0500

    👤 Create User SQLAlchemy model with user type field

---
+++ Backend/user-authentication-service/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    user_type = Column(String)
