12c0a62ad9c5b000175395babf01a34c41a6afab
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 00:12:11 -0500

    📄 Add user_controller.py

--- Backend/user-registration-service/app/controllers/user_controller.py
+++ Backend/user-registration-service/app/controllers/user_controller.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.schemas.user_schema import UserRegisterRequest
 from app.services.user_service import UserService
+from app.database.dependencies import get_db
 
-router = APIRouter(prefix="/users")
+router = APIRouter()
 
-@router.post("/register")
-def register_user(data: dict = Body(...)):
+@router.post("/register", status_code=201)
+def register_user(
+    request: UserRegisterRequest,
+    db: Session = Depends(get_db)
+):
     return UserService.register_user(
-        data["full_name"],
-        data["email"]
+        db=db,
+        full_name=request.full_name,
+        email=request.email,
+        password=request.password
     )
