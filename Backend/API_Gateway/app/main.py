a49e49425f55f4ca7cca5b25430f1c1dfee836cb
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 23:47:33 -0500

    🚀 Update API Gateway main application

--- Backend/API_Gateway/app/main.py
+++ Backend/API_Gateway/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routes import users
+from app.middlewares.auth_middleware import auth_middleware
+from app.middlewares.logging import logging_middleware
+from app.routes import auth, users, appointments
 
-app = FastAPI(
-    title="API Gateway",
-    description="Central API Gateway for Microservices",
-    version="1.0.0"
-)
+app = FastAPI(title="API Gateway")
 
-app.include_router(users.router, prefix="/users", tags=["Users"])
+app.middleware("http")(logging_middleware)
+app.middleware("http")(auth_middleware)
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(appointments.router)
