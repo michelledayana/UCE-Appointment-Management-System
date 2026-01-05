dbfc1c5aaf224c36c1ba56513a4ed30e9755ffb3
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 23:47:26 -0500

    ⚙️ Update configuration for API Gateway

--- Backend/API_Gateway/app/config.py
+++ Backend/API_Gateway/app/config.py
@@ -1,6 +1,14 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
+class Settings(BaseSettings):
+    AUTH_SERVICE_URL: str = "http://localhost:8082"
+    USER_SERVICE_URL: str = "http://localhost:8081"
+    APPOINTMENT_SERVICE_URL: str = "http://localhost:8083"
 
-USER_SERVICE_URL = os.getenv("USER_SERVICE_URL")
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
