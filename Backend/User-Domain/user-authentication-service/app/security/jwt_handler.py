e0d2ef3ea20b0b29959c354651e363009e7f63e2
michelledayana <michurivadeneira98@gmail.com>
2026-01-04 23:51:34 -0500

    🔐 Add JWT and password security utilities

---
+++ Backend/User-Domain/user-authentication-service/app/security/jwt_handler.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from app.config import settings
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.JWT_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
