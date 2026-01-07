460a531e7379cb9a351cf92bfbe1e2fc98000010
michelledayana <michurivadeneira98@gmail.com>
2026-01-07 12:55:08 -0500

    ⚡ feat(reg): implement user service logic

--- Backend/User-Domain/user-registration-service/app/services/user_service.py
+++ Backend/User-Domain/user-registration-service/app/services/user_service.py
@@ -3,30 +3,33 @@ from fastapi import HTTPException, status
 from app.models.user_model import User
 from app.repositories.user_repository import UserRepository
 from app.core.security import hash_password
+from app.messaging.kafka_producer import publish_user_registered_event
 
 class UserService:
-
     @staticmethod
     def register_user(db: Session, full_name: str, email: str, password: str):
-        existing_user = UserRepository.get_by_email(db, email)
-        if existing_user:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Email already registered"
-            )
+        # Check uniqueness
+        if UserRepository.get_by_email(db, email):
+            raise HTTPException(status_code=400, detail="Email already registered")
 
+        # UCE Requirement: Automatic classification
         user_type = "STUDENT" if email.endswith("@uce.edu.ec") else "GENERAL"
 
-        user = User(
+        new_user = User(
             full_name=full_name,
             email=email,
             password_hash=hash_password(password),
             user_type=user_type
         )
+        UserRepository.save(db, new_user)
 
-        UserRepository.save(db, user)
-
-        return {
-            "message": "User registered successfully",
-            "user_type": user_type
+        # Send data to Kafka for the Auth Service to consume
+        event_payload = {
+            "user_id": new_user.id,
+            "email": new_user.email,
+            "password_hash": new_user.password_hash,
+            "user_type": new_user.user_type
         }
+        publish_user_registered_event(event_payload)
+
+        return {"message": "User registered successfully", "user_type": user_type}
