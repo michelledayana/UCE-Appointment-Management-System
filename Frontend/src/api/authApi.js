3f33436c9fd0d44a464c7ff76c8fd4d7c28d8b70
michelledayana <michurivadeneira98@gmail.com>
2026-01-07 12:55:16 -0500

    🌐 feat(frontend): add auth API service

--- Frontend/src/api/authApi.js
+++ Frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081";
+const API_URL = "http://localhost:8082";
 
 export const registerUser = (data) =>
   axios.post(`${API_URL}/register`, data);
