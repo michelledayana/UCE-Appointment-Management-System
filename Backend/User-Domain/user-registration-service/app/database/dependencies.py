6e87cee7b429c9c18160377dda597dee3ae22f40
michelledayana <michurivadeneira98@gmail.com>
2026-01-07 16:43:02 -0500

    📦 chore(reg): update database dependencies

--- Backend/User-Domain/user-registration-service/app/database/dependencies.py
+++ Backend/User-Domain/user-registration-service/app/database/dependencies.py
@@ -1,15 +1,11 @@
 from app.database.db import SessionLocal
-from fastapi import HTTPException, status
 
 def get_db():
-    if SessionLocal is None:
-        raise HTTPException(
-            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail="Database not available"
-        )
-
     db = SessionLocal()
     try:
         yield db
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
