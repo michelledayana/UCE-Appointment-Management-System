26237929be76265838e21dafdbc26a35baef6c75
michelledayana <michurivadeneira98@gmail.com>
2026-01-09 21:00:03 -0500

    feat: 🔍 implement query to fetch services

--- Backend/Service-Domain/Catalog-services/app/application/queries/get_services.py
+++ Backend/Service-Domain/Catalog-services/app/application/queries/get_services.py
@@ -1,8 +1,20 @@
 from app.infrastructure.db.mongo import services_collection
+from app.infrastructure.redis.cache import (
+    get_services_cache,
+    set_services_cache
+)
+
 
 def get_services_query():
-    services = []
-    for service in services_collection.find():
-        service["_id"] = str(service["_id"])
-        services.append(service)
+    # 1️⃣ Try cache first
+    cached_services = get_services_cache()
+    if cached_services:
+        return cached_services
+
+    # 2️⃣ Query MongoDB
+    services = list(services_collection.find({}, {"_id": 0}))
+
+    # 3️⃣ Store in cache
+    set_services_cache(services)
+
     return services
