9bff11b76d666d1f4ef9ea35b1d78602e92c4daf
michelledayana <michurivadeneira98@gmail.com>
2026-01-09 20:59:26 -0500

    feat: 📋 update schedule data schemas

--- Backend/Scheduling-Domain/scheduling-management-service/app/schemas/schedule_schema.py
+++ Backend/Scheduling-Domain/scheduling-management-service/app/schemas/schedule_schema.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from datetime import date
 
+
 class ScheduleCreate(BaseModel):
     service_id: str
     date: date
     time_slot: str
+
 
 class ScheduleResponse(BaseModel):
     id: str
