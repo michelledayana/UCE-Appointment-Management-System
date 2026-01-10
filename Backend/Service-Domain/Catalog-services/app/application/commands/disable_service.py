08f0d879dc091856172aa9937ea3067289cf89e8
michelledayana <michurivadeneira98@gmail.com>
2026-01-09 21:00:03 -0500

    feat: 🚫 implement command to disable services

--- Backend/Service-Domain/Catalog-services/app/application/commands/disable_service.py
+++ Backend/Service-Domain/Catalog-services/app/application/commands/disable_service.py
@@ -1,4 +1,6 @@
 from app.infrastructure.db.mongo import services_collection
+from app.infrastructure.kafka.producer import publish_service_event
+from app.infrastructure.redis.cache import clear_services_cache
 from bson import ObjectId
 
 def disable_service_command(service_id: str):
@@ -10,6 +12,12 @@ def disable_service_command(service_id: str):
     if result.matched_count == 0:
         raise ValueError("Service not found")
 
+    publish_service_event("SERVICE_DISABLED", {
+        "id": service_id
+    })
+    
+    clear_services_cache()
+   
     return {
         "id": service_id,
         "is_active": False,
