b7ecb33137d03979111697add67aeab8b80c296b
michelledayana <michurivadeneira98@gmail.com>
2026-01-09 21:00:19 -0500

    feat: 📥 update kafka consumer for auth service

--- Backend/User-Domain/user-authentication-service/app/services/kafka_consumer.py
+++ Backend/User-Domain/user-authentication-service/app/services/kafka_consumer.py
@@ -1,14 +1,12 @@
 import json
 from kafka import KafkaConsumer
 from app.config import settings
-from app.database.db import SessionLocal
-from app.models.auth_model import AuthUser
 
 
 def iniciar_consumidor():
     consumer = KafkaConsumer(
-        "user_registered_topic",
-        bootstrap_servers=settings.kafka_bootstrap_servers.split(","),
+        settings.KAFKA_TOPIC_NAME,
+        bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS.split(","),
         value_deserializer=lambda m: json.loads(m.decode("utf-8")),
         group_id="auth-service-group",
         auto_offset_reset="earliest"
@@ -18,33 +16,4 @@ def iniciar_consumidor():
 
     for message in consumer:
         data = message.value
-        print("📨 User registered event:", data)
-
-        db = SessionLocal()
-        try:
-            # Evitar duplicados
-            existing = db.query(AuthUser).filter(
-                AuthUser.email == data["email"]
-            ).first()
-
-            if existing:
-                print(f"⚠️ User already exists in auth DB: {data['email']}")
-                continue
-
-            user = AuthUser(
-                email=data["email"],
-                password_hash=data["password_hash"],
-                user_type=data["user_type"]
-            )
-
-            db.add(user)
-            db.commit()
-
-            print(f"✅ User saved in auth DB: {user.email}")
-
-        except Exception as e:
-            db.rollback()
-            print("❌ Error saving user in auth DB:", e)
-
-        finally:
-            db.close()
+        print("📨 User registered event received:", data)
