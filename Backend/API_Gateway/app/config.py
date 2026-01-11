8f99b036cda2d1f707a03995614d69b3accb7b3b
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:51:41 -0500

    feat: ⚙️ update app configuration

--- Backend/API_Gateway/app/config.py
+++ Backend/API_Gateway/app/config.py
@@ -1,15 +1,35 @@
 from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    AUTH_SERVICE_URL: str = "http://localhost:8082"
-    USER_SERVICE_URL: str = "http://localhost:8081"
-    APPOINTMENT_SERVICE_URL: str = "http://localhost:8083"
-    CATALOG_SERVICE_URL: str = "http://localhost:8084"
 
-    JWT_SECRET_KEY: str
-    JWT_ALGORITHM: str
+class Settings(BaseSettings):
+    # JWT
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRE_MINUTES: int = 60
+
+    # USER DOMAIN
+    USER_REGISTRATION_URL: str
+    AUTH_SERVICE_URL: str
+    USER_PROFILE_URL: str
+
+    # SERVICE DOMAIN
+    SERVICE_CATALOG_URL: str
+
+    # SCHEDULING DOMAIN
+    SCHEDULING_MANAGEMENT_URL: str
+    AVAILABILITY_SERVICE_URL: str
+
+    # APPOINTMENT DOMAIN
+    APPOINTMENT_CREATION_URL: str
+    APPOINTMENT_MANAGEMENT_URL: str
+    APPOINTMENT_QUERY_URL: str
+
+    # ADMIN DOMAIN
+    ADMINISTRATION_SERVICE_URL: str
 
     class Config:
         env_file = ".env"
+        extra = "forbid"   # 👈 esto está bien, nos protege
+
 
 settings = Settings()
