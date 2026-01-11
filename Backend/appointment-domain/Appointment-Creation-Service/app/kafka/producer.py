38875fde0b8f32e0439450674f5d2f42496b086d
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:52:44 -0500

    feat: 📤 update Kafka producer for events

--- Backend/appointment-domain/Appointment-Creation-Service/app/kafka/producer.py
+++ Backend/appointment-domain/Appointment-Creation-Service/app/kafka/producer.py
@@ -2,10 +2,24 @@ import json
 from kafka import KafkaProducer
 from app.config import settings
 
-producer = KafkaProducer(
-    bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
-    value_serializer=lambda v: json.dumps(v).encode()
-)
+producer = None
+
+def get_producer():
+    global producer
+    if producer is None:
+        producer = KafkaProducer(
+            bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+    return producer
 
 def publish_appointment_created(event: dict):
-    producer.send("appointment_created", event)
+    try:
+        producer = get_producer()
+        producer.send(
+            settings.KAFKA_TOPIC_APPOINTMENT_CREATED,
+            event
+        )
+        producer.flush()
+    except Exception as e:
+        print("⚠ Kafka not available:", e)
