98b221f5e70d79d110ffbc768fb34cf6712a671e
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:52:44 -0500

    feat: 🗄️ update database connection logic

--- Backend/appointment-domain/Appointment-Creation-Service/app/db/database.py
+++ Backend/appointment-domain/Appointment-Creation-Service/app/db/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from app.config import settings
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://appointment_user:appointment_pass@db-appointments:5432/appointments_db"
-)
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
