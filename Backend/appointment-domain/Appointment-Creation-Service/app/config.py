fd58a61d3b92f2db61d39f6bd7bd0eede843ec5b
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:52:43 -0500

    feat: ⚙️ update service configuration

--- Backend/appointment-domain/Appointment-Creation-Service/app/config.py
+++ Backend/appointment-domain/Appointment-Creation-Service/app/config.py
@@ -1,8 +1,17 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    DATABASE_URL: str
+    # Kafka
     KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
+    KAFKA_TOPIC_APPOINTMENT_CREATED: str = "appointment.created"
+
+    # MQTT
     MQTT_BROKER: str = "hivemq"
     MQTT_PORT: int = 1883
+    MQTT_TOPIC_APPOINTMENT_CREATED: str = "appointments/created"
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
