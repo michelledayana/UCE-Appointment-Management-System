e77ae9c22830f59949400598f1f2edf36d6e3e50
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:52:32 -0500

    feat: 🔑 enhance JWT handler logic

--- Backend/API_Gateway/app/security/jwt_handler.py
+++ Backend/API_Gateway/app/security/jwt_handler.py
@@ -1,14 +1,12 @@
 from jose import jwt, JWTError
-from fastapi import HTTPException
 from app.config import settings
 
-def verify_token(token: str) -> dict:
+def decode_token(token: str):
     try:
-        payload = jwt.decode(
+        return jwt.decode(
             token,
-            settings.JWT_SECRET_KEY,
+            settings.JWT_SECRET,
             algorithms=[settings.JWT_ALGORITHM]
         )
-        return payload
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+        return None
