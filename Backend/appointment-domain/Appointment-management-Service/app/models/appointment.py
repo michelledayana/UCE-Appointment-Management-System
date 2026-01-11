18a5f4597cfb5fb63a6f245ca863c9ec701efcf3
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:54:10 -0500

    feat: 🏛️ update appointment database models

--- Backend/appointment-domain/Appointment-management-Service/app/models/appointment.py
+++ Backend/appointment-domain/Appointment-management-Service/app/models/appointment.py
@@ -1,17 +1,15 @@
+import uuid
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
-import uuid
-
-from app.db.database import Base
-
+from app.db.base import Base
 
 class Appointment(Base):
     __tablename__ = "appointments"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True)
     user_id = Column(String, nullable=False)
     service_id = Column(String, nullable=False)
     scheduled_time = Column(DateTime, nullable=False)
-    status = Column(String, default="CREATED")
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    status = Column(String, nullable=False)
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
