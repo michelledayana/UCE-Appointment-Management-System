0ca00bfbaf31e67cfad276396e2084accc0f0147
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:51:42 -0500

    refactor: 🚀 update main entry point

--- Backend/API_Gateway/app/main.py
+++ Backend/API_Gateway/app/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
-from app.middlewares.auth_middleware import auth_middleware
-from app.middlewares.logging import logging_middleware
-from app.routes import auth, users, appointments
+from app.config import settings
 
-app = FastAPI(title="API Gateway")
+from app.routes import (
+    auth_router,
+    users_router,
+    profile_router,
+    service_router,
+    scheduling_router,
+    appointment_router,
+    admin_router,
+)
 
-app.middleware("http")(logging_middleware)
-app.middleware("http")(auth_middleware)
+app = FastAPI(title=settings.PROJECT_NAME)
 
-app.include_router(auth.router)
-app.include_router(users.router)
-app.include_router(appointments.router)
+# ======================
+# ROUTERS
+# ======================
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
+app.include_router(profile_router, prefix="/profiles", tags=["Profiles"])
+app.include_router(service_router, prefix="/catalog", tags=["Catalog"])
+app.include_router(scheduling_router, prefix="/scheduling", tags=["Scheduling"])
+app.include_router(appointment_router, prefix="/appointments", tags=["Appointments"])
+app.include_router(admin_router, prefix="/admin", tags=["Admin"])
+
+
+@app.get("/health")
+def health():
+    return {"status": "API Gateway OK"}
