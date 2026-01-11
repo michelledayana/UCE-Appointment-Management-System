2f3161a394dc2b97c6fbbf673857bc527b9c1200
michelledayana <michurivadeneira98@gmail.com>
2026-01-11 16:51:55 -0500

    feat: 🕒 add new scheduling module

---
+++ Backend/API_Gateway/app/routes/scheduling.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+import requests
+from app.config import settings
+
+router = APIRouter()
+
+@router.post("/")
+def create_schedule(data: dict):
+    return requests.post(
+        f"{settings.SCHEDULING_SERVICE_URL}/schedules/schedules/",
+        json=data
+    ).json()
+
+@router.get("/health")
+def availability_health():
+    return requests.get(
+        f"{settings.AVAILABILITY_SERVICE_URL}/health"
+    ).json()
