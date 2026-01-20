ffc0fd4e3774c289b0f177c49591befb53ed8b13
michelledayana <michurivadeneira98@gmail.com>
2026-01-20 16:15:17 -0500

    feat(admin): ⚙️ update app configuration and database connection

--- Backend/Administration-Domain/Administration-service/app/config.py
+++ Backend/Administration-Domain/Administration-service/app/config.py
@@ -8,16 +8,24 @@ SERVICE_NAME = os.getenv("SERVICE_NAME", "administration-service")
 # ===============================
 # MongoDB
 # ===============================
-MONGO_HOST = os.getenv("MONGO_HOST", "mongodb_admin")
-MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "admin_db")
-MONGO_URI = os.getenv(
-    "MONGO_URI",
-    f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
-)
+
+if not MONGO_URI:
+    raise RuntimeError(
+        "MONGO_URI is required. "
+        "Define it in the environment variables."
+    )
 
 # ===============================
 # Kafka
 # ===============================
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
-KAFKA_TOPIC_EVENTS = os.getenv("KAFKA_TOPIC_EVENTS", "system_events")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv(
+    "KAFKA_BOOTSTRAP_SERVERS",
+    "kafka:9092"
+)
+
+KAFKA_TOPIC_EVENTS = os.getenv(
+    "KAFKA_TOPIC_EVENTS",
+    "system_events"
+)
